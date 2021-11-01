9ec9666b016ce1269ecb564b883dc5ceb03188e1
neverbeenthisweeb <rf.ramadhana@gmail.com>
2021-11-01 23:16:01 +0700

    Complete pt. 43, 44, 45, 46, and 47

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash_input()
     |> pick_color()
+    |> build_grid()
   end
 
   def hash_input(input) do
@@ -13,9 +14,33 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(image) do
-    # Pattern matching for struct and list
-    %Identicon.Image{hex: [r, g, b | _]} = image
-    [r, g, b]
+  def pick_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  @doc """
+  Mirrors a row.
+
+  `mirror_row/1` only supports `row` with a length of 3.
+
+  ## Examples
+
+      iex> Identicon.mirror_row([1, 2, 3])
+      [1, 2, 3, 2, 1]
+  """
+  def mirror_row(row) do
+    [f, s | _] = row
+    row ++ [s, f]
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    g =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: g}
   end
 end
